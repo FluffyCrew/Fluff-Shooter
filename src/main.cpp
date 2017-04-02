12732e388acdeea7e23d960b8727c20e79fbb444
Nilxoc <colin.gebler@gebler-online.de>
2017-04-02 18:23:34 +0200

    Donezo

--- src/main.cpp
+++ src/main.cpp
@@ -13,24 +13,60 @@ int main(int argc, char *argv[])
     SDL_Window* window = NULL;
     SDL_Surface* screenSurface = NULL;
 
-    if(SDL_Init(SDL_INIT_VIDEO) < 0){
-      printf("SDL could not initialize. SDL_Error: %s\n", SDL_GetError());
-      return 1;
+    if(SDL_Init(SDL_INIT_VIDEO) < 0)
+    {
+        printf("SDL could not initialize. SDL_Error: %s\n", SDL_GetError());
+        return 1;
     }
     else
     {
         window = SDL_CreateWindow( "SDL Tutorial", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
         if(window == NULL)
         {
-             printf("Window could not be created. SDL_Error: %s\n", SDL_GetError());
-             return 1;
+            printf("Window could not be created. SDL_Error: %s\n", SDL_GetError());
+            return 1;
         }
         else
         {
             screenSurface = SDL_GetWindowSurface(window);
-            SDL_FillRect(screenSurface, NULL, SDL_MapRGB(screenSurface -> format, 0xFF, 0xFF, 0xFF));
-            SDL_UpdateWindowSurface(window);
-            SDL_Delay(2000);
+            SDL_Renderer *ren = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
+            if (ren == NULL)
+            {
+                SDL_DestroyWindow(window);
+                printf("SDL_CreateRenderer Error: %s\n",SDL_GetError());
+                SDL_Quit();
+                return 1;
+            }
+            SDL_Surface *bmp = SDL_LoadBMP((string(SDL_GetBasePath()) + string("\\FrogInACar.bmp")).c_str());
+            if (bmp == NULL)
+            {
+                SDL_DestroyRenderer(ren);
+                SDL_DestroyWindow(window);
+                printf("SDL_LoadBMP Error: %s\n",SDL_GetError());
+                SDL_Quit();
+                return 1;
+            }
+            SDL_Texture *tex = SDL_CreateTextureFromSurface(ren, bmp);
+            if (tex == NULL)
+            {
+                SDL_DestroyRenderer(ren);
+                SDL_DestroyWindow(window);
+                printf("SDL_CreateTextureFromSurface Error: %s\n",SDL_GetError());
+                SDL_Quit();
+                return 1;
+            }
+            SDL_FreeSurface(bmp);
+            for (int i = 0; i < 3; ++i)
+            {
+                //First clear the renderer
+                SDL_RenderClear(ren);
+                //Draw the texture
+                SDL_RenderCopy(ren, tex, NULL, NULL);
+                //Update the screen
+                SDL_RenderPresent(ren);
+                //Take a quick break after all that hard work
+                SDL_Delay(1000);
+            }
         }
     }
 
